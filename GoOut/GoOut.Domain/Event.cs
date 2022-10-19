ce48fab8b75945e5242a22cbc6cb63bb617629ea
Alex Stoianovici <astoianovici@ymail.com>
2022-10-19 15:33:28 +0300

    Event

---
+++ GoOut/GoOut.Domain/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoOut.Domain
+{
+    class Event
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string EntryFee { get; set; }
+    }
+}
