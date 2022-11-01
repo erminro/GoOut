b65f7b9ca4e5fe4e7b8143774b0bb303e35278ea
Ermin <romanermin@gmail.com>
2022-11-01 19:54:47 +0200

    Added UnitOfWork

---
+++ GoOut/GoOut.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using GoOut.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoOut.Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DataContext _appDbContext;
+
+        public async Task<int> SaveChangesAsync()
+        {
+
+            return await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
