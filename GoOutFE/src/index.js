bee9e11498d3b5c1518a2c2a7bb5daaf6aafd9ed
Alex Stoianovici <astoianovici@ymail.com>
2022-11-23 13:40:32 +0200

    navbar still doesn't work

--- GoOutFE/src/index.js
+++ GoOutFE/src/index.js
@@ -9,63 +9,19 @@ import HomeOutlined from 'reactjs-bottom-navigation'
 import SearchOutlined from 'reactjs-bottom-navigation'
 import BellOutlined from 'reactjs-bottom-navigation'
 import MenuOutlined from 'reactjs-bottom-navigation'
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import FolderIcon from '@mui/icons-material/Folder';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-// function App() {
-//   // items
 
-//   const bottomNavItems = [
-//     {
-//       title: 'Home',
 
-//       icon: <HomeOutlined style={{ fontSize: '18px' }} />,
-
-//       activeIcon: <HomeOutlined style={{ fontSize: '18px', color: '#fff' }} />
-//     },
-
-//     {
-//       title: 'Search',
-
-//       icon: <SearchOutlined style={{ fontSize: '18px' }} />,
-
-//       activeIcon: <SearchOutlined style={{ fontSize: '18px', color: '#fff' }} />
-//     },
-
-//     {
-//       title: 'Notifications',
-
-//       icon: <BellOutlined style={{ fontSize: '18px' }} />,
-
-//       activeIcon: <BellOutlined style={{ fontSize: '18px', color: '#fff' }} />
-//     },
-
-//     {
-//       title: 'Menu',
-
-//       icon: <MenuOutlined style={{ fontSize: '18px' }} />,
-
-//       activeIcon: <MenuOutlined style={{ fontSize: '18px', color: '#fff' }} />,
-
-//       onClick: () => alert('menu clicked')
-//     }
-//   ]
-
-//   return (
-//     <div>
-//       <BottomNavigation
-//         items={bottomNavItems}
-//         selected={0}
-//         onItemClick={(item) => console.log(item)}
-//       />
-//     </div>
-//   )
-// }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(LabelBottomNavigation());
+ReactDOM.render(LabelBottomNavigation, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
