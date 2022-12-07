a9f2967cb1c3b506ed721508806cfa190821c9a1
Alex Stoianovici <astoianovici@ymail.com>
2022-12-07 16:32:52 +0200

    Added Login and Register page

--- GoOutFE/src/index.js
+++ GoOutFE/src/index.js
@@ -3,25 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import BottomNavigation from 'reactjs-bottom-navigation'
-import 'reactjs-bottom-navigation/dist/index.css'
-import HomeOutlined from 'reactjs-bottom-navigation'
-import SearchOutlined from 'reactjs-bottom-navigation'
-import BellOutlined from 'reactjs-bottom-navigation'
-import MenuOutlined from 'reactjs-bottom-navigation'
-import * as React from 'react';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import FolderIcon from '@mui/icons-material/Folder';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(LabelBottomNavigation());
-ReactDOM.render(LabelBottomNavigation, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
