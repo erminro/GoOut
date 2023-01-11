bb3e0d5d6da0ab0e682b3263b0d7ae1ca123581b
Alex Stoianovici <astoianovici@ymail.com>
2023-01-11 18:14:06 +0200

    added states to navbar

--- GoOutFE/src/Navbar.jsx
+++ GoOutFE/src/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BottomNavigation  from 'reactjs-bottom-navigation'
 import { HomeOutlined }  from '@ant-design/icons'
 import { SearchOutlined }  from '@ant-design/icons'
@@ -7,29 +7,43 @@ import { MenuOutlined }  from '@ant-design/icons'
 import {SettingOutlined } from '@ant-design/icons'
 import 'reactjs-bottom-navigation/dist/index.css'
 
+const HandleClick = (state) => {
+  // counter++;
+  // console.log("counter", counter);
+  // renderContent();
+  useState(state)
+};
+
 export const Navbar = () => {
+  // const toggleForm = (formName) => {
+  //   setCurrentForm(formName);
+  // }
+
 const bottomNavItems = [
     {
       icon: <HomeOutlined style={{ fontSize: '18px', color: '#000'  }} />,
-      activeIcon: <HomeOutlined style={{ fontSize: '18px', color: '#000' }} />
+      activeIcon: <HomeOutlined style={{ fontSize: '18px', color: '#000' }} />,
+      onClick: () => HandleClick('home')
     },
     {
       icon: <SearchOutlined style={{ fontSize: '18px', color: '#000' }} />,
-      activeIcon: <SearchOutlined style={{ fontSize: '18px', color: '#000' }} />
+      activeIcon: <SearchOutlined style={{ fontSize: '18px', color: '#000' }} />,
+      onClick: () => HandleClick('search')
     },
     {
       icon: <PlusOutlined style={{ fontSize: '18px', color: '#000' }} />,
-      activeIcon: <PlusOutlined style={{ fontSize: '18px', color: '#000' }} />
+      activeIcon: <PlusOutlined style={{ fontSize: '18px', color: '#000' }} />,
+      onClick: () => HandleClick('plus')
     },
     {
       icon: <MenuOutlined style={{ fontSize: '18px', color: '#000' }} />,
       activeIcon: <MenuOutlined style={{ fontSize: '18px', color: '#000' }} />,
-      //onClick: () => alert('menu clicked')
+      onClick: () => HandleClick('menu')
     },
     {
         icon: <SettingOutlined style={{ fontSize: '18px', color: '#000' }} />,
         activeIcon: <SettingOutlined style={{ fontSize: '18px', color: '#000' }} />,
-        //onClick: () => alert('menu clicked')
+        onClick: () => HandleClick('login')
       }
   ]
   return(
